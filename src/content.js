76b85defb3fd532d7c24d1186f58545c727201f8
Jae <one.sweet.dreams@gmail.com>
2023-04-17 00:49:49 -0400

    cambios de header

--- src/content.js
+++ src/content.js
@@ -2,46 +2,52 @@ export const header =`<header class="encabezado">
 
 <div class="contenedor1">
   <div id="logo">
-
     <img src="./img/riot-game-blanco-logo.png" alt="logo-riot" id="logo-riot">
   
     <img src="./img/lol-logo-chibi.png" alt="logo-lol" id="logo-lol">
-
   </div>
 
   <nav class="nav1">
     <ul class="links">
-      <li><a href="#"></a>Eventos</li>
-      <li><a href="#"></a>Noticias</li>
-      <li><a href="#"></a>Campeones</li>
-      <li><a href="#"></a>Productos</li>
+      <li><a href="#">Eventos</a></li>
+      <li><a href="#">Noticias</a>
+      <ul class="submenu">
+      <li><a href="#">Historias</a></li>
+      <li><a href="#">Multimedia</a></li>
+      <li><a href="#">Comunidad</a></li>
+      <li><a href="#">Esports</a></li>
+      <li><a href="#">Updates de LOL</a></li>
+    </ul>
+      </li>
+      <li><a href="#">Campeones</a></li>
+      <li><a href="#">Productos</a></li>
     </ul>
   </nav>
-</div>
+  </div>
+  <div class="contenedor2"> 
 
-<div class="contenedor2">
-  <nav class="nav2">
-    <ul class="sesion">
-      <li><a href="#"><img src="./icon/iniciodesesion-2px.png" class="inicio-sesion" alt="icon"></a>Inicio de sesion</li>
-    </ul>
-  </nav>
+  <nav class="nav2"> 
+
+    <div class="contenedor-busqueda">
+      <div class="buscador">
+
+        <form>
+          <input type="search" placeholder="Buscar">
+          <button type="button"><img src="./icon/search.png" alt="lupa" class="search"></button>
+        </form>
+
+      </div>
+    </div>
+
+    <ul class="sesion"> 
+      <li><a href="#"><img src="./icon/iniciodesesion-2px.png" class="inicio-sesion" alt="icon">Inicio de sesion</a></li>
+    </ul> 
+
+  </nav> 
+
+    </div> 
+
 </div>
 
 </header>`
 
-export const footer = `<footer>
-<nav class="navfooter">
-  <ul class="redessociales">
-    <li><a href="#"></a><img src="./icon/instagram.png" alt=""></li>
-    <li><a href="#"></a><img src="./icon/youtube.png" alt=""></li>
-    <li><a href="#"></a><img src="./icon/facebook.png" alt=""></li>
-    <li><a href="#"></a><img src="./icon/gorjeo.png" alt=""></li>
-    <li><a href="#"></a><img src="./icon/discord.png" alt=""></li>
-  </ul>
-
-  <div class="logo-riot2">
-    <img src="./img/riot-game-blanco-logo.png" alt="logo-riot" id="logo-riot2">
-  </div>
-</nav>
-
-</footer>`
