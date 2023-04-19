16593964807c6bc9eac873556e91d81807c86c93
Jae <one.sweet.dreams@gmail.com>
2023-04-18 20:41:48 -0400

    commit

--- src/content.js
+++ src/content.js
@@ -1,30 +1,42 @@
-/*export const header =`<header class="encabezado">
+export const header =`<header class="encabezado">
 
 <div class="contenedor1">
-  <div id="logo">
+  <div id="logo1">
     <img src="./img/riot-game-blanco-logo.png" alt="logo-riot" id="logo-riot">
-  
     <img src="./img/lol-logo-chibi.png" alt="logo-lol" id="logo-lol">
   </div>
-
-  <nav class="nav1">
-    <ul class="links">
-      <li><a href="#">Eventos</a></li>
-      <li><a href="#">Noticias</a>
-      <ul class="submenu">
-      <li><a href="#">Historias</a></li>
-      <li><a href="#">Multimedia</a></li>
-      <li><a href="#">Comunidad</a></li>
-      <li><a href="#">Esports</a></li>
-      <li><a href="#">Updates de LOL</a></li>
-    </ul>
-      </li>
-      <li><a href="#">Campeones</a></li>
-      <li><a href="#">Productos</a></li>
-    </ul>
-  </nav>
+  <div class="contenedor-responsive">
+  <div id="logo2">
+    <img src="./img/LoL-logo.png" alt="logo-LoL" id="logo-lol-mediano">
+  </div> 
   </div>
-  <div class="contenedor2"> 
+
+  <div class="menu">
+    <input type="checkbox" id="check__menu"> <!--al tener el mismo ID con FOR, le doy una misma funcion-->
+    <label for="check__menu" id="label__menu">
+        <i class="icon__menu"><img src="./icon/menu-2px.png" alt="menu" id="img-menu"></i>
+      </label> <!--fas fa-bars i class + lo que ya tiene con espacio-->
+    
+    <nav <!class="nav1">
+      <ul <!class="links">
+        <li><a href="#">Eventos</a></li>
+        <li><a href="#">Noticias</a>
+            <ul class="submenu"> 
+                <li><a href="#">Historias</a></li>
+                <li><a href="#">Multimedia</a></li>
+                <li><a href="#">Comunidad</a></li>
+                <li><a href="#">Esports</a></li>
+                <li><a href="#">Updates de LOL</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Campeones</a></li>
+        <li><a href="#">Productos</a></li>
+      </ul>
+    </nav>
+  </div>
+
+</div>
+<div class="contenedor2"> 
 
   <nav class="nav2"> 
 
@@ -41,13 +53,11 @@
 
     <ul class="sesion"> 
       <li><a href="#"><img src="./icon/iniciodesesion-2px.png" class="inicio-sesion" alt="icon">Inicio de sesion</a></li>
-    </ul> 
+    </ul>
 
   </nav> 
 
     </div> 
 
-</div>
-
-</header>`*/
+</header>`
 
